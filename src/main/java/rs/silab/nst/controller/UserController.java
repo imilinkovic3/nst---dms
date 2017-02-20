37aa76a0195b7bad94a9b485d71e8410a4e17f09
INT\kuzmanom <m.kuzmanovic@msgnetconomy.net>
2017-02-20 17:01:21 +0100

    Admin home page

---
+++ src/main/java/rs/silab/nst/controller/UserController.java
@@ -0,0 +1,43 @@
+package rs.silab.nst.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import rs.silab.nst.model.User;
+import rs.silab.nst.service.RoleService;
+import rs.silab.nst.service.UserService;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * Created by milinkoi on 11.02.2017.
+ */
+
+@Controller
+@Scope("session")
+@RequestMapping("/nst/login")
+public class UserController {
+
+    @Resource(name = "roleService")
+    RoleService roleService;
+
+    @Resource(name = "userService")
+    UserService userService;
+
+
+    @RequestMapping(value = {"/editUser/"}, method = RequestMethod.POST)
+    public String editUser(@RequestBody User user) throws IOException {
+        userService.updateUser(user);
+        return "homepage_admin";
+    }
+
+    @RequestMapping(value = {"/selectedUser/"}, method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody User selectUser(Integer id) {
+        User selectedUser = userService.findById(id);
+        return selectedUser;
+    }
+}
---
+++ src/main/webapp/js/tableUsers.js
@@ -0,0 +1,92 @@
+function delete_row(id) {
+    document.getElementById("confirmationType").value = "delete";
+    document.getElementById("rowId").value = id;
+    modalConfirmation.style.display = "block";
+}
+
+function add_row() {
+    document.getElementById("confirmationType").value = "addRow";
+    var new_firstname = document.getElementById("new_firstname").value;
+    var new_lastname = document.getElementById("new_lastname").value;
+    var new_username = document.getElementById("new_username").value;
+
+    if (!new_firstname) {
+        $('#new_firstname_error').html("Please provide firstname");
+        $('#new_firstname_error').show();
+        return;
+    } else {
+        $('#new_firstname_error').hide();
+    }
+
+    if (!new_lastname) {
+        $('#new_lastname_error').html("Please provide lastname");
+        $('#new_lastname_error').show();
+        return;
+    } else {
+        $('#new_lastname_error').hide();
+    }
+
+    if (!new_username) {
+        $('#new_username_error').html("Please provide username");
+        $('#new_username_error').show();
+        return;
+    } else {
+        $('#new_username_error').hide();
+    }
+
+    modalConfirmation.style.display = "block";
+}
+
+function save_row() {
+    var new_firstname = document.getElementById("new_firstname").value;
+    var new_username = document.getElementById("new_username").value;
+    var new_lastname = document.getElementById("new_lastname").value;
+
+    // var table = document.getElementById("data_table");
+    // var table_len = (table.rows.length) - 1;
+
+    // Setting value to hidden element of table. We will use this value to recognize which row fit on selected user
+    // document.getElementById("rowId").value = table_len;
+
+    save_new_user(new_firstname, new_lastname, new_username);
+
+    // var row = table.insertRow(table_len).outerHTML =
+    //     "<tr id='row" + table_len + "'>" +
+    //     "<td id='firstname_row" + table_len + "'>" + new_firstname + "</td>" +
+    //     "<td id='lastname_row" + table_len + "'>" + new_lastname + "</td>" +
+    //     "<td id='username_row" + table_len + "'>" + new_username + "</td>" +
+    //     "<td>" +
+    //     " <input type='button' id='edit_button" + table_len + "' value='Edit' class='edit btn btn-primary btn-xs' onclick='edit(" + table_len + ")'>" +
+    //     " <input type='button' value='Delete' class='delete btn btn-primary btn-xs' onclick='delete_row(" + table_len + ")'>" +
+    //     "</td>" +
+    //     "</tr>";
+
+    document.getElementById("new_firstname").value = "";
+    document.getElementById("new_username").value = "";
+    document.getElementById("new_lastname").value = "";
+}
+
+function transformRowUserToJson(new_firstname, new_lastname, new_username) {
+    var dataUser = JSON.stringify({
+        "firstname": new_firstname,
+        "lastname": new_lastname,
+        "username": new_username,
+    });
+    return dataUser;
+}
+
+function save_new_user(new_firstname, new_lastname, new_username) {
+    var user = transformRowUserToJson(new_firstname, new_lastname, new_username);
+    $.ajax({
+        url: "/nst/login/editUser/",
+        type: 'POST',
+        data: user,
+        contentType: 'application/json',
+        success: function (data) {
+            location.reload();
+        },
+        error: function (data, status, er) {
+            console.log(data + " " + status + " " + er);
+        }
+    });
+}
